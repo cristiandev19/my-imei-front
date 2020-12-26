a25691ca4202056f5e890cf17e6f0416e125f525
pystudent1913 <cristiansotomayor1912@gmail.com>
2020-12-26 10:56:17 -0500

    ahora perfil tiene algo

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import { Container, makeStyles } from '@material-ui/core'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../auth/AuthContext';
+import { ProfileForm } from '../ProfileForm/ProfileForm';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    }
+  },
+  paper: {
+    height: 140,
+    width: 100,
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
+  form: {
+    width: '60%',
+    margin: 'auto'
+  }
+}));
 
 export const Profile = () => {
-    return (
-        <div>
-            hola estas en el profile
+  const classes = useStyles();
+
+  const { user } = useContext(AuthContext);
+  console.log("🚀 ~ file: Profile.js ~ line 32 ~ Profile ~ user", user)
+
+  const handleActionForm = (data) => {
+    console.log("🚀 ~ file: Profile.js ~ line 35 ~ actionForm ~ data", data)
+
+  }
+  return (
+    <>
+      <Container fixed className={classes.main}>
+
+        <div className={classes.form}>
+          <h2> Aqui puedes modificar tu informacion de perfil </h2>
+          <ProfileForm user={ user } actionForm={ handleActionForm } />
         </div>
-    )
+      </Container>
+    </>
+  )
 }
