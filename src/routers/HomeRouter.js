11728184298703dd1278f6a4fe404db31824bacc
pystudent1913 <cristiansotomayor1912@gmail.com>
2020-12-26 10:57:28 -0500

    agregue perfil

--- src/routers/HomeRouter.js
+++ src/routers/HomeRouter.js
@@ -6,16 +6,18 @@ import {
 } from "react-router-dom";
 import { HeaderHome } from '../components/HeaderHome/HeaderHome';
 import { Home } from '../components/Home/Home';
+import { Profile } from '../components/Profile/Profile';
 
 export const HomeRouter = ({ history }) => {
+
+
 
   return (
     <>
       <HeaderHome />
       <Switch>
-        <Route  path="" component={ Home } />
-
-        {/* <Route exact component={ Home } /> */}
+        <Route exact path="/profile" component={ Profile } />
+        <Route exact path="" component={ Home } />
 
       </Switch>
     </>
