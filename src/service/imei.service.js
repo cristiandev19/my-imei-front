ffff1e49cd8cee94d852ccd4b8e5e85f6bac0a39
pystudent1913 <cristiansotomayor1912@gmail.com>
2020-12-29 00:50:49 -0500

    nuevo servicio

--- src/service/imei.service.js
+++ src/service/imei.service.js
@@ -36,3 +36,49 @@ export const addImei = async (imeiData) => {
   }
 
 }
+
+export const deleteImei = async (_id_imei) => {
+  try {
+    const fetch_data = await fetch(`${URL}/imei/deleteImei`, {
+      "method": "DELETE",
+      "headers": {
+        "Content-Type": "application/json"
+      },
+      // "headers": {
+      //   "cookie": "PHPSESSID=2rjqso2ukk193osc73r8uvmk2p",
+      //   "Content-Type": "application/json"
+      // },
+      "body": JSON.stringify({ _id_imei })
+    })
+    if(fetch_data.status === 400) {
+      throw new Error('fallo');
+    }
+    const data = await fetch_data.json();
+    return data;
+  } catch (error) {
+  }
+
+}
+
+export const deletupdateImeieImei = async (imeiData) => {
+  try {
+    const fetch_data = await fetch(`${URL}/imei/updateImei`, {
+      "method": "PUT",
+      "headers": {
+        "Content-Type": "application/json"
+      },
+      // "headers": {
+      //   "cookie": "PHPSESSID=2rjqso2ukk193osc73r8uvmk2p",
+      //   "Content-Type": "application/json"
+      // },
+      "body": JSON.stringify({ ...imeiData })
+    })
+    if(fetch_data.status === 400) {
+      throw new Error('fallo');
+    }
+    const data = await fetch_data.json();
+    return data;
+  } catch (error) {
+  }
+
+}
